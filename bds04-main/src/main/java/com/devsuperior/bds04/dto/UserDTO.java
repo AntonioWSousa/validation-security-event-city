dcc5154468db299eb92bbe29d149f1c7daceaa3e
Antonio Sousa <antoniowsj@outlook.com>
2021-11-19 22:02:17 -0300

    Validation and security commit

---
+++ bds04-main/src/main/java/com/devsuperior/bds04/dto/UserDTO.java
@@ -0,0 +1,57 @@
+package com.devsuperior.bds04.dto;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import com.devsuperior.bds04.entities.User;
+
+public class UserDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	@NotBlank(message = "Campo obrigatório")
+	
+	@Email(message = "Favor entrar com um email válido")
+	private String email;
+
+	Set<RoleDTO> roles = new HashSet<>();
+	
+	public UserDTO() {
+	}
+
+	public UserDTO(Long id, String firstName, String lastName, String email, String password) {
+		this.id = id;
+		this.email = email;
+	}
+	
+	public UserDTO(User entity) {
+		id = entity.getId();
+		email = entity.getEmail();
+		entity.getRoles().forEach(role -> this.roles.add(new RoleDTO(role)));
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Set<RoleDTO> getRoles() {
+		return roles;
+	}
+
+}
